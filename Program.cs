4d9fce29f4e8f6af1cd6648d082c0cb77c8396e8
Rafael César(RaYaS) <rafael.cesar.souza@hotmail.com>
2025-06-22 17:24:17 -0300

    resolução

--- Program.cs
+++ Program.cs
@@ -13,7 +13,8 @@ builder.Services.AddControllers().AddJsonOptions(options =>
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
